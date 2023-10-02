f0b84c41add1fdb11232bd0115e89df6e1f7ad3f
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-10-02 10:34:01 +0900

    1-1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+template<class T>
+T Min(T x, T y) {
+	return x < y ? x : y;
+}
+
+void Min(char x, char y) {
+	std::cout << "”ŽšˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ" << std::endl;
+}
+
 int main() {
+	int32_t x_int = 114, y_int = 514;
+	float x_float = 514.0f, y_float = 1140.0f;
+	double x_double = 810.0, y_double = 1919.0;
+	char x_char = 'a', y_char = 'i';
+
+	std::cout << Min(x_int, y_int) << std::endl;
+	std::cout << Min(x_float, y_float) << std::endl;
+	std::cout << Min(x_double, y_double) << std::endl;
+	Min(x_char, y_char);
 
 	return 0;
 }
