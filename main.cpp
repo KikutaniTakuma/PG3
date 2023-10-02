3eb31cdb4622f55bbb9672f0f99ad042e77c6849
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-10-02 11:51:18 +0900

    計算ミスを修正

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 ﻿#include <iostream>
 #include <fstream>
 
-int32_t SalaryCalculation(int32_t num, int32_t recursive) {
+int32_t SalaryCalculation(int32_t num, int32_t recursive, int32_t total) {
 	static int32_t general = 1072;
-	if ((num * general) < recursive) {
+	if ((num * general) < total) {
 		return num;
 	}
 
 	num++;
-	recursive += (recursive * 2) - 50;
-	return SalaryCalculation(num, recursive);
+	recursive = (recursive * 2) - 50;
+	total += recursive;
+	return SalaryCalculation(num, recursive, total);
 }
 
 int main() {
 	std::ofstream file{ "./answer.md" };
-	int32_t answer = SalaryCalculation(1, 100);
+	int32_t answer = SalaryCalculation(1, 100, 100);
 
 	std::cout << answer << std::endl;
 
