3ad4423a3c411ef58d6792a25b3ab01c7a09d6c9
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-10-02 11:16:29 +0900

    関数の名前変更

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
 
-int32_t OtinginKudasaiFunction(int32_t num, int32_t recursive) {
+int32_t SalaryCalculation(int32_t num, int32_t recursive) {
 	static int32_t general = 1072;
 	if ((num * general) < recursive) {
 		return num;
@@ -9,12 +9,12 @@ int32_t OtinginKudasaiFunction(int32_t num, int32_t recursive) {
 
 	num++;
 	recursive += (recursive * 2) - 50;
-	return OtinginKudasaiFunction(num, recursive);
+	return SalaryCalculation(num, recursive);
 }
 
 int main() {
 	std::ofstream file{ "./answer.md" };
-	int32_t answer = OtinginKudasaiFunction(1, 100);
+	int32_t answer = SalaryCalculation(1, 100);
 
 	std::cout << answer << std::endl;
 
