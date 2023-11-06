ed93a87430d6ce4c4a1f53b71a877b8e238eb255
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-11-06 10:59:48 +0900

    04_01

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
+#include <Windows.h>
+#include <string>
 
 int main() {
+	// この関数を使うと文字化けする
+	//SetConsoleOutputCP(65001);
+
+	std::string myLowerBackHurts = "腰が限界";
+
+	printf(myLowerBackHurts.c_str());
 
 	return 0;
 }
