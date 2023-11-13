1676d02f11d0425cfff6be6850e8e07f69590707
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-11-13 13:04:54 +0900

    仕様変更に対応

--- main.cpp
+++ main.cpp
@@ -16,14 +16,22 @@ int32_t Dice() {
 
 
 int main() {
-	std::function<void(bool)> answer = [](bool isCorrect) {
-		std::cout << std::endl;
-		int32_t count = 3;
+	std::function<void(int32_t)> setTimeOut = [](int32_t second) {
+		int32_t count = second;
 		while (count > 0) {
 			std::cout << count << std::endl;
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			count--;
 		}
+		};
+
+	std::wcout << "何秒待つか入力してください" << std::endl;
+	int32_t waitSecond = 0;
+	std::cin >> waitSecond;
+
+	std::function<void(bool)> answer = [waitSecond, &setTimeOut](bool isCorrect) {
+		std::cout << std::endl;
+		setTimeOut(waitSecond);
 
 		if (isCorrect) {
 			std::wcout << "正解!!!" << std::endl;
