ad9be89493b61ec953a539a4c0a06769bc06c746
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-01-29 10:05:52 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
+#include <chrono>
+#include <string>
 
 int main() {
+	std::string str(100000, 'a' );
+
+	auto startTime = std::chrono::steady_clock::now();
+
+	std::string copyStr = str;
+
+	auto endTime = std::chrono::steady_clock::now();
+
+	std::cout << "copy : " << std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime) << std::endl;
+
+
+	startTime = std::chrono::steady_clock::now();
+
+	std::string moveStr = std::move(str);
+
+	endTime = std::chrono::steady_clock::now();
+
+	std::cout << "move : " << std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime) << std::endl;
 
 	return 0;
 }
